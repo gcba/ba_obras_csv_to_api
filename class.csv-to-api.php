800db182bd360474f8dba0446c2f4ab77eba5e2c
Pablo H. Paladino <paladinop@gmail.com>
2016-05-16 17:01:19 -0400

    preg match callback

--- class.csv-to-api.php
+++ class.csv-to-api.php
@@ -429,7 +429,7 @@ class CSV_To_API {
   function jsonp_callback_filter( $callback ) {
 
     // As per <http://stackoverflow.com/a/10900911/1082542>.
-    if ( preg_match( '/[^0-9a-zA-Z\$_]|^(abstract|boolean|break|byte|case|catch|char|class|const|continue|debugger|default|delete|do|double|else|enum|export|extends|false|final|finally|float|for|function|goto|if|implements|import|in|instanceof|int|interface|long|native|new|null|package|private|protected|public|return|short|static|super|switch|synchronized|this|throw|throws|transient|true|try|typeof|var|volatile|void|while|with|NaN|Infinity|undefined)$/', $callback) ) {
+    if ( preg_match( '/[^0-9a-zA-Z\$_.]|^(abstract|boolean|break|byte|case|catch|char|class|const|continue|debugger|default|delete|do|double|else|enum|export|extends|false|final|finally|float|for|function|goto|if|implements|import|in|instanceof|int|interface|long|native|new|null|package|private|protected|public|return|short|static|super|switch|synchronized|this|throw|throws|transient|true|try|typeof|var|volatile|void|while|with|NaN|Infinity|undefined)$/', $callback) ) {
       return false;
     }
 
